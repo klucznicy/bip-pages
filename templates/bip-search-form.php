f2667f9d632067a477347a766caf3a1b6229fa52
Lucas Garczewski <lgarczewski@gmail.com>
2019-09-02 02:32:12 +0200

    added missing language domain for i18n

--- templates/bip-search-form.php
+++ templates/bip-search-form.php
@@ -1,12 +1,12 @@
 <form role="search" method="get" action="<?= esc_url( home_url( '/' ) ) ?>">
 	<label>
   	<span class="screen-reader-text">
-			<?= esc_html_x( 'Search for:', 'label' ) ?>
+			<?= esc_html_x( 'Search for BIP pages:', 'label', 'bip-pages' ) ?>
 		</span>
 		<input type="search"
-			placeholder="<?= esc_attr_x( 'Search BIP pages&hellip;', 'placeholder' ) ?>"
+			placeholder="<?= esc_attr_x( 'Search BIP pages&hellip;', 'placeholder', 'bip-pages' ) ?>"
 			value="<?= get_search_query() ?>" name="s" />
 	</label>
-	<input type="submit" value="<?= esc_attr_x( 'Search', 'submit button' ) ?>" />
+	<input type="submit" value="<?= esc_attr_x( 'Search', 'submit button', 'bip-pages' ) ?>" />
 	<input type="hidden" name="post_type" value="bip" />
 </form>

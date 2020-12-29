9c918d140da8e53a5992cc016724119db8194fe0
Lucas Garczewski <lgarczewski@gmail.com>
2020-12-29 20:13:54 +0100

    notices no longer needed after conversion to blocks

--- bip-pages-main-page.php
+++ bip-pages-main-page.php
@@ -20,15 +20,6 @@ function add_basic_main_page_data_fallback( $content ) {
 }
 add_filter('the_content', __NAMESPACE__ . '\add_basic_main_page_data_fallback' );
 
-function main_page_edit_notice() {
-  if ( is_bip_main_page_edit_screen() ) {
-    $message = '<p>' . esc_html__( 'You are editing the BIP main page.', 'bip-pages' ) . '</p>' .
-      '<p>' . esc_html__( "This page needs to include the BIP logo, your organization's address, a BIP search module, and a link to the instruction page.", 'bip-pages' ) . '</p>';
-    echo "<div class='notice is-info is-dismissible'>{$message}</div>";
-  }
-}
-add_action( 'admin_notices', __NAMESPACE__ . '\main_page_edit_notice' );
-
 function is_bip_main_page_edit_screen() {
   return isset( $_GET['action'] ) &&
       $_GET['action'] == 'edit' &&
